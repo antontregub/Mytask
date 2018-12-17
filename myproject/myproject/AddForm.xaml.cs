1c6e53dcf7f4794f011d07f8becdefc1ff96bce0
antontregub <esesoves@gmail.com>
2018-12-17 15:36:18 +0200

    myproject

---
+++ myproject/myproject/AddForm.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace myproject
+{
+    /// <summary>
+    /// Логика взаимодействия для AddForm.xaml
+    /// </summary>
+    public partial class AddForm : Window
+    {
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///Function for add workwrs
+        /// </summary>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime? bda = datePicker1.SelectedDate;
+            DateTime bday = bda.Value;
+            DateTime now = DateTime.Today;
+            int age = now.Year - bday.Year;
+            if (now < bday.AddYears(age)) age--;
+            MainWindow.People.Add(new Person() { Name = txtName.Text, Position = txtAddress.Text, Age = age });
+            txtName.Text = string.Empty;
+            txtAddress.Text = string.Empty;
+        }
+    }
+}
---
+++ myproject/myproject/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace myproject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<Person> People { get; set; }
+        ObservableCollection<Person> peopleSort ;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            People = new ObservableCollection<Person>
+            {
+                new Person { Name="Toxa", Position="Junior" },
+                new Person { Name="Oleh", Position="Midl" },
+                new Person { Name="Bohdan", Position="Admin" },
+            };
+            Sort();
+        }
+
+        /// <summary>
+        /// Open form for add workers
+        /// </summary>
+        private void AddForm(object sender, RoutedEventArgs e)
+        {
+            AddForm addform = new AddForm();
+            addform.Show();
+        }
+
+        /// <summary>
+        /// Sort workers for name
+        /// </summary>
+        public void Sort()
+        {
+            peopleSort = new ObservableCollection<Person>(People.OrderBy(Person => Person.Name));
+            workers.ItemsSource = peopleSort;
+        }
+
+        /// <summary>
+        /// Delete worker
+        /// </summary>
+        private void Del(object sender, RoutedEventArgs e)
+        {
+            Button cmd = (Button)sender;
+            if (cmd.DataContext is Person)
+            {
+                Person deleteme = (Person)cmd.DataContext;
+                People.Remove(deleteme);
+                Sort();
+            }
+        }
+
+        /// <summary>
+        /// Refresh list after add
+        /// </summary>
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            Sort();
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; } // Name
+        public string Position { get; set; } // Position              
+        public int Age { get; set; } //Age
+        public string Datastart { get; set; }
+        public string Datefinish { get; set; }
+    }
+}
